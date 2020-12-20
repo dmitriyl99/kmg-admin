23ce8bca0adc31a1e9795fd05b760754ddf8b964
dmitriyl99 <dmitriyl1899@gmail.com>
2020-12-20 13:13:10 +0500

    Images polymorphic model

---
+++ database/migrations/2020_12_20_075404_create_invoices_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('active')->default(false);
+            $table->date('invoice_date')->nullable();
+            $table->string('container_size');
+            $table->string('company');
+            $table->integer('total_due')->default(0);
+            $table->text('shipper')->nullable();
+            $table->text('consignee')->nullable();
+
+            $table->foreignId('container_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
