50c182ff55f858022149f3d77b93256e827d2f97
dmitriyl99 <dmitriyl1899@gmail.com>
2020-12-20 14:53:58 +0500

    Create Container API Controller

---
+++ app/Http/Controllers/Api/ContainerController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Helpers\ImageHelper;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ContainerRequest;
+use App\Models\Container;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+
+class ContainerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $containers = Container::query()->paginate(15);
+        return response()->json($containers);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  ContainerRequest  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(ContainerRequest $request)
+    {
+        $data = $request->validated();
+        if (in_array('paid', $data))
+            $data['paid'] = true;
+        if (in_array('active', $data['invoice']))
+            $data['invoice']['active'] = true;
+
+        $container = DB::transaction(function () use ($data, $request) {
+            /** @var Container $container */
+            $container = Container::query()->create($data);
+
+            $container->invoice()->create($data['invoice']);
+
+            $images = $request->file('images');
+            foreach ($images as $image) {
+                $imageData = ImageHelper::saveImage($image, 'containers');
+                $container->images()->create($imageData);
+            }
+
+            return $container;
+        });
+
+        return response()->json($container);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $container = Container::with('images', 'invoice', 'status')->findOrFail($id);
+
+        return response()->json($container);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  ContainerRequest  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(ContainerRequest $request, $id)
+    {
+        /** @var Container $container */
+        $container = Container::query()->findOrFail($id);
+
+        $data = $request->validated();
+        if (in_array('paid', $data))
+            $data['paid'] = true;
+        if (in_array('active', $data['invoice']))
+            $data['invoice']['active'] = true;
+
+        $container = DB::transaction(function () use ($data, $request, $container) {
+            $container->update($data);
+
+            $container->invoice->update($data['invoice']);
+
+            if ($request->has('images')) {
+                foreach ($container->images as $image)
+                    ImageHelper::deleteImage($image->filename);
+                $container->images()->delete();
+
+                $images = $request->file('images');
+                foreach ($images as $image) {
+                    $imageData = ImageHelper::saveImage($image, 'containers');
+                    $container->images()->create($imageData);
+                }
+            }
+
+            return $container;
+        });
+
+        return response()->json($container);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        /** @var Container $container */
+        $container = Container::query()->findOrFail($id);
+
+        DB::transaction(function () use ($container) {
+            foreach ($container->images as $image) {
+                ImageHelper::deleteImage($image->filename);
+            }
+            $container->images()->delete();
+            $container->invoice->delete();
+            $container->delete();
+        });
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ app/Http/Requests/ContainerRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContainerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'container_number' => 'required|string',
+            'booking_number' => 'nullable|string',
+            'shipping_line' => 'nullable|string',
+            'container_status_id' => 'required|integer|in:container_statuses,id',
+            'port' => 'required|string',
+            'destination' => 'required|string',
+            'capacity' => 'required|integer',
+            'paid' => 'nullable',
+            'tracking_link' => 'nullable|string',
+            'departure_date' => 'nullable|date',
+            'arrival_date' => 'nullable|date',
+
+            'images' => 'nullable|array',
+            'images.*' => 'mimes:jpg,jpeg,png|max:5120',
+
+            'invoice.active' => 'nullable',
+            'invoice.date' => 'nullable|date',
+            'invoice.container_size' => 'required|string',
+            'invoice.company' => 'required|string',
+            'invoice.total_due' => 'required|number',
+            'invoice.shipper' => 'required|string',
+            'invoice.consignee' => 'required|string'
+        ];
+    }
+}
