c63bb6cccdaf7076d454810be73d7a15a51f696f
dmitriyl99 <dmitriyl1899@gmail.com>
2021-01-01 18:50:25 +0500

    Add name and images to cargo

--- app/Models/Cargo.php
+++ app/Models/Cargo.php
@@ -10,7 +10,7 @@ class Cargo extends Model
     use HasFactory;
 
     protected $fillable = [
-        'owner', 'vin', 'port', 'received_at', 'destination_country',
+        'owner', 'name', 'vin', 'port', 'received_at', 'destination_country',
         'transportation_from', 'transportation_address', 'transportation_price',
         'date_received', 'key',
         'total_due', 'postal_service', 'tracking_number', 'container_id'
@@ -20,13 +20,18 @@ class Cargo extends Model
         'date_received', 'received_at'
     ];
 
-    public function container()
+    public function container(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Container::class);
     }
 
-    public function transportation()
+    public function transportation(): \Illuminate\Database\Eloquent\Relations\HasOne
     {
         return $this->hasOne(Transportation::class);
+    }
+
+    public function images(): \Illuminate\Database\Eloquent\Relations\MorphMany
+    {
+        return $this->morphMany(Image::class, 'imageable');
     }
 }
