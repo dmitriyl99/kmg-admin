b05137cf4818b6dfdd6bf858262129b33ac52c4e
dmitriyl99 <dmitriyl1899@gmail.com>
2021-01-11 01:12:10 +0500

    Routes for transportation CRUD

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -11,10 +11,15 @@ export default [
   { path: '/containers/create', component: page('containers/create.vue'), name: 'containers.create' },
   { path: '/containers/:id', component: page('containers/show.vue'), name: 'containers.show' },
   { path: '/containers/:id/edit', component: page('containers/edit.vue'), name: 'containers.edit' },
+
   { path: '/cargos', component: page('cargos/index.vue'), name: 'cargos.index' },
   { path: '/cargos/create', component: page('cargos/create.vue'), name: 'cargos.create' },
   { path: '/cargos/:id/edit', component: page('cargos/edit.vue'), name: 'cargos.edit' },
   { path: '/cargos/:id', component: page('cargos/show.vue'), name: 'cargos.show' },
+
+  { path: '/transportations', component: page('transportations/index.vue'), name: 'transportations.index' },
+  { path: '/transportations/create', component: page('transportations/create.vue'), name: 'transportations.create' },
+  { path: '/transportations/:id/edit', component: page('transportations/edit.vue'), name: 'transportations.edit' },
   {
     path: '/settings',
     component: page('settings/index.vue'),
