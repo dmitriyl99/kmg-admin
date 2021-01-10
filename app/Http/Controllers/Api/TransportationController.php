5e26a747048385427deb74f88b55a7c50f0702aa
dmitriyl99 <dmitriyl1899@gmail.com>
2021-01-11 01:13:22 +0500

    CRUD for transportations

--- app/Http/Controllers/Api/TransportationController.php
+++ app/Http/Controllers/Api/TransportationController.php
@@ -5,16 +5,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TransportationRequest;
 use App\Models\Transportation;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TransportationController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\JsonResponse
+     * @param Request $request
+     * @return JsonResponse
      */
-    public function index(Request $request)
+    public function index(Request $request): JsonResponse
     {
         $paginate = $request->query('paginate', true);
         $query = Transportation::query();
@@ -26,10 +29,10 @@ class TransportationController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\JsonResponse
+     * @param TransportationRequest $request
+     * @return JsonResponse
      */
-    public function store(TransportationRequest $request)
+    public function store(TransportationRequest $request): JsonResponse
     {
         $data = $request->validated();
         $transportation = Transportation::query()->create($data);
@@ -40,33 +43,40 @@ class TransportationController extends Controller
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function show($id)
+    public function show(int $id): JsonResponse
     {
-        //
+        $transportation = Transportation::query()->findOrFail($id);
+        return response()->json($transportation);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param TransportationRequest $request
+     * @param int $id
+     * @return JsonResponse
      */
-    public function update(Request $request, $id)
+    public function update(TransportationRequest $request, int $id): JsonResponse
     {
-        //
+        $data = $request->validated();
+        $transportation = Transportation::query()->findOrFail($id);
+        $transportation->update($data);
+
+        return response()->json($transportation);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse
      */
-    public function destroy($id)
+    public function destroy(int $id): JsonResponse
     {
-        //
+        Transportation::destroy($id);
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
